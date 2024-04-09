9ed021b093009bcced6299b9ff0db73f3853295a
Manu <manuedavakandam@gmail.com>
2024-04-09 22:48:36 +1000

    query

--- huggingface_pinecone.py
+++ huggingface_pinecone.py
@@ -1,25 +1,22 @@
 import os
 from pinecone import Pinecone, PodSpec
-
 from llama_index.llms.huggingface import (
     HuggingFaceInferenceAPI,
     HuggingFaceLLM,
 )
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.vector_stores.pinecone import PineconeVectorStore
+from llama_index.core import  Settings
+from llama_index.core.storage import StorageContext
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+
 HF_TOKEN = os.environ.get("HUGGING_FACE_TOKEN", '<HUGGINGFACE_TOKEN>')
 remotely_run = HuggingFaceInferenceAPI(
     model_name="HuggingFaceH4/zephyr-7b-alpha", token=HF_TOKEN
 )
 
 # locally_run = HuggingFaceLLM(model_name="HuggingFaceH4/zephyr-7b-alpha")
-
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-from llama_index.vector_stores.pinecone import PineconeVectorStore
-from llama_index.core import  Settings
-
-# initialize without metadata filter
-from llama_index.core.storage import StorageContext
-
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 Settings.embed_model = embed_model
@@ -48,7 +45,9 @@ documents = SimpleDirectoryReader("./data/paul_graham").load_data()
 
 vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
 index = VectorStoreIndex.from_documents(
     documents, storage_context=storage_context, show_progress=True
 )
+
+query_engine = index.as_query_engine()
+response = query_engine.query("What I Worked On?")
